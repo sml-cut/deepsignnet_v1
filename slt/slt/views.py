e467f2e7ff163492e430133fa0f7310e353e72c3
partaourides <harrispartaourides@gmail.com>
2020-12-21 18:30:39 +0200

    basic rest api functionality

--- slt/slt/views.py
+++ slt/slt/views.py
@@ -24,13 +24,15 @@ def web_video_upload(request):
 def rest_video_upload(request):
     if request.method == "POST" and request.FILES["video_file"]:
         video_file = request.FILES["video_file"]
+        request_details = request.POST['request_details']
         fs = FileSystemStorage()
         filename = fs.save('received/' + video_file.name, video_file)
         video_url = fs.url(filename)
         print(video_url)
         return JsonResponse({
-            "video_url": video_url,
-            "video_translation": "This is a dummy video translation",
+            "video_stored_at": video_url,
+            "request_details": request_details,
+            "sign_language_translation": "This is a dummy video translation",
         })
     return JsonResponse({'API_type': 'GET'})
 
